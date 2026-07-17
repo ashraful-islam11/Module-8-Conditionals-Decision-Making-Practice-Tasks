bd08d394d168b6abd66dfbdbc1e9fa6090b6038e
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 21:53:43 +0600

    All logical operator use case

---
+++ task-1.js
@@ -0,0 +1,73 @@
+// practice task: 1. 
+let prise1 = 97;
+let prise2 = 66;
+
+//   use == operator : 
+if(prise1 == prise2){
+    console.log(`${prise1} is  equal to ${prise2}`);
+}
+else{
+    console.log(`${prise1} is not equal to ${prise2}`);
+}
+
+// use === operator: 
+
+if(prise1 === prise2){
+    console.log(`${prise1} is  equal to ${prise2}`);
+
+}
+else{
+    console.log(`${prise1} is not equal to ${prise2}`);
+}
+
+// grater than operator: > 
+
+if(prise1 > prise2){
+    console.log(`${prise1} is greater than ${prise2}`);
+}
+else{
+    console.log(`${prise2} is is greater than ${prise1}`);
+}
+
+
+// less than operator : < 
+if(prise1 < prise2){
+    console.log(`${prise2} is greater than ${prise1}`);
+}else{
+    console.log(`${prise2} is less than ${prise1}`);
+}
+
+//  grater than or equal : >= 
+
+if(prise1 >= prise2){
+    console.log(`${prise1} is greater than ${prise2}`);
+}else{
+    console.log(`${prise2} is is greater than ${prise1}`);
+}
+
+
+// less than or equal : <=
+if(prise1 <= prise2){
+    console.log(`${prise2} is greater than ${prise1}`);
+}else{
+    console.log(`${prise2} is less than ${prise1}`);
+}
+
+
+// not equal to operator : 
+
+if( prise1 != prise2){
+    console.log(' truethy ');
+} else{
+    console.log('falsy');
+}
+
+//  double equal to operator : 
+
+if( prise1 !== prise2){
+    console.log(' truethy ');
+} else{
+    console.log('falsy');
+}
+
+
