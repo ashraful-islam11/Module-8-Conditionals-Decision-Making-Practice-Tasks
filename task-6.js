f7f640876a2b12a4c7b46366e9589261c30f95dd
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 22:44:37 +0600

    task-6 , leap year is done

---
+++ task-6.js
@@ -0,0 +1,8 @@
+// todo:       topic: Take a year variable and check whether it is a leap year (hint: year % 4 === 0).
+
+const year = 2026;
+if(year % 4 === 0){
+    console.log(`${year} is leap year `);
+}else{
+    console.log(`${year} is not a leap year`);
+}
