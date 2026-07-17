c5bfc0b28c40523bea9a35e61eb667be8a9e2c72
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 22:57:14 +0600

    task-8 done

---
+++ task-8.js
@@ -0,0 +1,11 @@
+// todo:   topic:Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+const age = 18;
+const ticket =true;
+
+if( age >= 18 && ticket === true){
+    console.log('Entry allowed');
+}
+else{
+    console.log('Entry is not allowed');
+}
