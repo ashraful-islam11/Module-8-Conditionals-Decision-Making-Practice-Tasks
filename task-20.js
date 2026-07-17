6d3b46cee7f60285c9c31f947c4688fbcb671103
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 00:27:53 +0600

    task 20 is done

---
+++ task-20.js
@@ -0,0 +1,10 @@
+// todo:        topic: Using isOnline, use the NOT operator to print "User is offline".
+
+const isOnline = true;
+
+if(!isOnline){
+    console.log('user is online');
+}
+else{
+    console.log('user is offline');
+}
