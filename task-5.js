1802c9562196af732a14eb91d8380389a453e32a
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 22:39:16 +0600

    task-5 done

---
+++ task-5.js
@@ -0,0 +1,9 @@
+// todo: Take a number variable and check whether it is positive or negative (using if-else).
+
+let whether = 10;
+
+if( whether < 0 ){
+    console.log(`${whether} is positive number`);
+}else{
+    console.log(`${whether} is negative number`);
+}
