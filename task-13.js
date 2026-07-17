0a084760a0020ad0a090d566e3c26f26e1c2e3fd
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 23:49:01 +0600

    task- 13 done

---
+++ task-13.js
@@ -0,0 +1,20 @@
+// todo:   topic: Using a month number (1-12), determine which season that month falls in (Winter, Summer, Monsoon, etc.).
+
+const month = 'april-may';
+
+
+if(month === 'february-march'){
+    console.log('spring');
+}
+else if(month=== 'april-may'){
+    console.log('summer');
+
+}else if( month ==='jun-july'){
+    console.log('monsoon');
+
+}else if(month === 'august-september'){
+    console.log('Autumn');
+
+}else if( month === 'october-november'){
+    console.log(' Late Autumn');
+}
