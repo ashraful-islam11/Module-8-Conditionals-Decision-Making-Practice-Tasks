2b9e075f8f6d1c83053a4e54c0af78dc71f00a6e
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 23:06:34 +0600

    task-9

---
+++ task-9.js
@@ -0,0 +1,11 @@
+// todo:   topic: Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+
+let isWeekend =true; 
+let isHoliday = true;
+
+if(isHoliday === true || isWeekend === true){
+        console.log('No work today');
+}
+else{
+    console.log(' i have lot of work today');
+}
