59c6baa66d42ae35a092a00d18b7614763136b48
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 00:12:50 +0600

    task 17 is done

---
+++ task-17.js
@@ -0,0 +1,6 @@
+// todo :       topic:  Using age, determine "Adult" or "Minor" using the ternary operator.
+
+const age = 75;
+
+const isAdult = (age >=18) ? `${age} is Adult`: `${age} is minor`;
+console.log(isAdult);
