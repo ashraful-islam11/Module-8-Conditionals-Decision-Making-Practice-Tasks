96037758d7dd87238f4abd453d6679ff0df30c06
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 23:13:10 +0600

    task -10

---
+++ task-10.js
@@ -0,0 +1,10 @@
+// todo:    topic: Build a simple login system — print "Login successful" only if both username and password are correct..
+
+const password = 123;
+const username ='ashraful';
+
+if( username === 'ashraful' && password === 123){
+    console.log('Login Successful');
+}else{
+    console.log('try again');
+}
