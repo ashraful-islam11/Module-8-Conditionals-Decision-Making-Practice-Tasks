2982951399be2402f564aeae56468128d61f0ac4
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-17 22:25:47 +0600

    task - 4 done

---
+++ task-4.js
@@ -0,0 +1,9 @@
+// todo:     topic: Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
+
+const stock = 0;
+if(stock === 0){
+    console.log(`${stock} Out of stock`);
+}
+else{
+    console.log('now i have some stock');
+}
