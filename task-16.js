d85a43affadcf28fb016e9aa1aefb557815ece8d
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-18 00:08:21 +0600

    task -16 is done

---
+++ task-16.js
@@ -0,0 +1,7 @@
+// todo:        topic:    Determine whether a number is even or odd using the ternary operator.
+
+const number = 5501;
+
+const isEvenOrIsOdd = (number % 2 === 0) ? `${number} is even number`: `${number} is odd number `;
+
+console.log(isEvenOrIsOdd);
